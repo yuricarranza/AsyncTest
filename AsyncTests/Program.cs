f9e2a2bb4f7c9f5aa653cb8121cd557370e7453b
Yuri carranza <alwaysyurixD@hotmail.com>
2021-06-06 01:21:31 -0500

    first commit

---
+++ AsyncTests/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncTests
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //5741
+            List<string> codes = new List<string> { "co", "pe", "es", "en",  "col", "co", "pe", "es", "en", "col" };
+            List<Task<string>> tasks = new List<Task<string>>();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            CountryService countryService = new CountryService();
+            foreach (var item in codes)
+            {
+                tasks.Add(countryService.GetPaisAsync(item));
+            }
+
+            var tasksProcessed = tasks.Select(async x =>
+            {
+                var json = await x;
+                Console.WriteLine("-------------------------\n");
+                Console.WriteLine(json);
+            }).ToArray();
+
+            await Task.WhenAll(tasksProcessed);
+
+            stopwatch.Stop();
+            Console.WriteLine($"Tiempo transcurrido: {stopwatch.ElapsedMilliseconds} milisegundos");
+            Console.ReadLine();
+        }
+    }
+
+    class CountryService
+    {
+        private readonly HttpClient httpClient;
+        public CountryService()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://restcountries.eu/rest/v2/alpha/");
+        }
+
+        public async Task<string> GetPaisAsync(string code)
+        {
+            var response = await httpClient.GetAsync(code);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ AsyncTests/Program2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncTests
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello World!");
+
+    //        Console.ReadLine();
+    //    }
+
+    //    static async Task DoSomething()
+    //    {
+    //        await Task.Delay(TimeSpan.FromSeconds(2));
+    //        Console.WriteLine("Hola denuevo");
+    //        throw new Exception("Error!");
+    //    }
+    //}
+
+
+    //public class Program
+    //{
+    //    static async Task Main(string[] args)
+    //    {
+    //        List<string> codes = new List<string> { "co", "col", "pe", "es", "co", "col", "pe", "es" };
+    //        CountrieService countrieService = new CountrieService();
+    //        Stopwatch stopwatch = new Stopwatch();
+    //        stopwatch.Start();
+    //        foreach (var item in codes)
+    //        {
+    //            var json = await countrieService.GetCountry(item);
+    //            Console.WriteLine($"{item}: {json}");
+    //        }
+    //        stopwatch.Stop();
+    //        Console.WriteLine($"Tiempo transcurrido: {stopwatch.ElapsedMilliseconds} milisegundos");
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    //public class Program2
+    //{
+    //    static async Task Main(string[] args)
+    //    {
+    //        List<string> codes = new List<string> { "co", "col", "pe", "es", "co", "col", "pe", "es" };
+    //        List<Task<string>> tasks = new List<Task<string>>();
+    //        CountrieService countrieService = new CountrieService();
+    //        Stopwatch stopwatch = new Stopwatch();
+    //        stopwatch.Start();
+    //        foreach (var item in codes)
+    //        {
+    //            tasks.Add(countrieService.GetCountry(item));
+    //        }
+
+    //        var processedTasks = tasks.Select(async x => {
+    //            string json = await x;
+    //            Console.WriteLine("--------------");
+    //            Console.WriteLine($"{json}");
+    //        }).ToArray();
+
+    //        await Task.WhenAll(processedTasks);
+    //        stopwatch.Stop();
+    //        Console.WriteLine($"Tiempo transcurrido: {stopwatch.ElapsedMilliseconds} milisegundos");
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    //public class CountrieService
+    //{
+    //    private readonly HttpClient httpClient;
+    //    public CountrieService()
+    //    {
+    //        httpClient = new HttpClient();
+    //        httpClient.BaseAddress = new Uri("https://restcountries.eu/rest/v2/alpha/");
+    //    }
+
+    //    public async Task<string> GetCountry(string code)
+    //    {
+    //        var response = await httpClient.GetAsync(code);
+    //        return await response.Content.ReadAsStringAsync();
+    //    }
+    //}
+
+}
